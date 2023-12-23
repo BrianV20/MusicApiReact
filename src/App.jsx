963821cc7cf69ba6d9d3057a901881c832c11723
Brian <brianezequielv2004@gmail.com>
2023-12-23 00:18:50 -0300

    ya solucioné lo de tailwind, y ya arranqué con los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
 import { useState, useEffect } from "react";
-import "./App.css";
-// import { getArtists } from "./services/Artist";
-// import { getArtist } from "./services/Artist";
-// import { createArtist } from "./services/Artist";
-// import { deleteArtist } from "./services/Artist";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // const [artists, setArtists] = useState([]);
-  // const [singleArtist, setSingleArtist] = useState({});
-
-  useEffect(() => {
-    // getArtists().then((data) => setArtists(data));
-    // getArtist(16).then((data) => setSingleArtist(data));
-    // createArtist({ name: "Pablo" }).then((data) => console.log(data));
-    // deleteArtist(27).then((data) => console.log(data));
-  }, []);
 
   return (
     <>
+    {/* <h1 className="text-3xl flex justify-center py-5">Hello world!</h1>  */}
     <NavBar />
-
-
-      {/* <div style={{ backgroundColor: "violet", padding: "1rem" }}>
-        <h2>Artistas: </h2>
-        {artists.map((artist) => {
-          return (
-            <h3 key={artist.id + artist.name}>
-              {artist.id} - {artist.name}
-            </h3>
-          );
-        })}
-      </div>
-
-      <div style={{ backgroundColor: "greenyellow", padding: "1rem" }}>
-        <h2>Artista por id</h2>
-        <h3>
-          {singleArtist.id} - {singleArtist.name}
-        </h3>
-      </div> */}
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import "../index.css";
 
 export default function NavBar() {
   return (
     <>
-      <nav>
-        <i className="fa-solid fa-compact-disc"></i>
+      <nav className="bg-green-800">
+        <div className="text-white flex justify-between border-2 border-pink-400 py-3 px-2">
+          <i className="fa-solid fa-bars text-xl flex items-center"></i>
+          <h1 className="w-full text-2xl pl-5 flex items-center">Popular</h1>
+          <i className="fa-solid fa-magnifying-glass px-3 flex items-center"></i>
+        </div>
+
+        <div className="text-white border-2 border-blue-400 px-2 flex justify-around text-center text-base">
+          <a href="#" className="navItem">Releases</a>
+          <a href="#" className="navItem">Reviews</a>
+          <a href="#" className="navItem">Lists</a>
+          <a href="#" className="navItem">News</a>
+        </div>
+        {/* <i className="fa-solid fa-compact-disc text-white"></i> */}
       </nav>
     </>
   );
